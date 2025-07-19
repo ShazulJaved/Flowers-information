944c71518b43be6ed53156beedd167fd71f4fbde
ShazulJaved <shazul03266646@gmail.com>
2025-07-19 16:53:35 +0500

    Add files via upload

---
+++ Flowers.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Flowers=(props) =>{
+    console.log(props);
+    return(
+        <>
+        <div className="cards">
+          <h2>{props.title}</h2>
+          <img src={props.imgsrc} alt="rose"></img>
+          <p>{props.desc}</p>
+          <button>{props.btn}</button>
+          <hr/>
+          </div>
+        </>
+    )
+}
+
+
+
+
+
+export default Flowers;
